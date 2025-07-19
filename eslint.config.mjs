43c536be253b8bea6d64a9b7534a71f9de0098f7
Sungsu Kim <spelar7@gmail.com>
2025-07-20 01:38:58 +0900

    chore: allow unused catch variables in ESLint config to prevent build failures

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Allow unused catch variables (ignore unused variables in catch blocks)
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_", "caughtErrors": "none" }],
+    },
+  },
 ];
 
 export default eslintConfig;
